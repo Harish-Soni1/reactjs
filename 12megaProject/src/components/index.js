6cf94ded2c47c953355766d5d88690e2fcd6e417
Harish <sonih277@gmail.com>
2024-01-05 21:31:37 +0530

    add some components

--- 12megaProject/src/components/index.js
+++ 12megaProject/src/components/index.js
@@ -1,7 +1,13 @@
+import Logo from "./Logo";
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
+import LogoutBtn from "./Header/LogoutBtn";
+import Container from "./container/Container";
 
 export {
+    Logo,
     Header,
-    Footer
+    Footer,
+    Container,
+    LogoutBtn
 }
