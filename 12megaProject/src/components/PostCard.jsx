971b5d001446ccb825d7a2add42601fe21669e4d
Harish <sonih277@gmail.com>
2024-01-06 12:44:13 +0530

    add button and input components

---
+++ 12megaProject/src/components/PostCard.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import appWriteService from "../appWrite/appWriteService"
+
+
+function PostCard() {
+    return (
+        <Link to={"/post"}>
+        
+        </Link>
+    )
+}
+
+export default PostCard
